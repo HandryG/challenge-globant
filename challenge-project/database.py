b24e1d0c1d6e1a04dacb4fa4b76c90fbafbabeeb
HandryG <handryguillenzapata@gmail.com>
2025-03-22 18:29:01 -0500

    change name folder

---
+++ challenge-project/database.py
@@ -0,0 +1,26 @@
+import psycopg2
+from psycopg2.extras import DictCursor
+from challenge-proyect.config import settings
+import os
+
+def get_connection():
+    """Get PostgreSQL connection."""
+    conn = psycopg2.connect(
+        settings.DATABASE_URL,
+        cursor_factory=DictCursor
+    )
+    return conn
+
+def get_sql_file_content(filename):
+    """Read SQL file content."""
+    sql_path = os.path.join(settings.SQL_DIR, filename)
+    with open(sql_path, 'r') as f:
+        return f.read()
+
+def execute_sql_file(conn, filename):
+    """Execute SQL file on database connection."""
+    cursor = conn.cursor()
+    sql = get_sql_file_content(filename)
+    cursor.execute(sql)
+    conn.commit()
+    cursor.close()
